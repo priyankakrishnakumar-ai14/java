c7c59f19374b950eab849cd67b15f49c054a2146
priyankakrishnakumar-ai14 <priyankakrishnakumar14@gmail.com>
2026-03-02 20:51:47 +0530

    Update and rename uc5.txt to uc5.java

--- uc5.java
+++ uc5.java
@@ -1,4 +1,4 @@
-public class usecase5 {
+public class uc5 {
     public static void main(String[] args) {
         String[] banner = {
             String.join("   ","   ***   ", "   ***   "," ***** ", "   **** "),
